55adcc7afc39e520cfec693e01582bc35a2daf74
Eletr0n1c <danielmoraessid@gmail.com>
2025-04-18 10:00:31 -0300

    first commit

---
+++ ProdutoApp.Infra.Data/Contexts/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProdutoApp.Infra.Data.Contexts
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ProdutoBD;Integrated Security=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
